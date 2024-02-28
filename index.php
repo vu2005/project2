a927ce069e5777c5a0d5e50280ec5a7d4d112f97
Your Name <your-email@example.com>
2024-02-29 01:10:17 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="header">
+
+        <div class="navbar">
+            <div class="nav">
+                <div class="logo">
+                    <a href="index.php"><img src="./images/logo.png" width="125px"></a>
+                </div>
+                <nav>
+                    <ul id="MenuItems">
+                        <li><a href="">Mua Xe <i class='bx bxs-chevron-down'></i> </a></li>
+                        <li><a href="">Bán Xe</a></li>
+                        <li><a href="">Giới Thiệu</a></li>
+                        <li><a href="">Tin Tức</a></li>
+                    </ul>
+                </nav>
+
+
+
+            </div>
+            <div class="customers">
+                <a href="/">
+                    <button class="btn btn-primary">
+                        <i class='bx bxs-car-mechanic'></i>
+                        Sửa Xe
+                    </button>
+                </a>
+                <div class="contact">
+                    <a href="/">
+                        <button class="btn btn-outline">
+                            <i class='bx bx-phone-call'></i>
+                            0384804325
+                        </button>
+                    </a>
+                </div>
+                <div class="user">
+                    <a href="/">
+                        <button class="btn btn-outline">
+                            <i class='bx bx-user'></i>
+                            <span>Tài Khoản</span> <i class='bx bxs-chevron-down'></i>
+                        </button>
+                    </a>
+                    <div class="nav2">
+                        <div class="box">
+                            <i class='bx bxs-checkbox'></i>
+
+                        </div>
+                        <ul>
+
+                            <li><a href="/">Đăng nhập</a></li>
+                            <li><a href="/">Đăng ký</a></li>
+                            <li><a href="/">Báo Giá</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="title"></div>
+    <div class="container">
+        <div class="card-img">
+            <img src="./images/card-title1.png" alt="">
+        </div>
+        <div class="card-img">
+            <img src="./images/card-title2.png" alt="">
+        </div>
+        <div class="card-img">
+            <img src="./images/card-title3.png" alt="">
+        </div>
+        <div class="card-img">
+            <img src="./images/card-title4.png" alt="">
+        </div>
+        <div class="card-img">
+            <img src="./images/card-title5.png" alt="">
+        </div>
+    </div>
+</body>
+
+</html>
